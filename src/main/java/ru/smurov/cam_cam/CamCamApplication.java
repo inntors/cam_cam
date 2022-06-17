cf3a3f02701ad132d1a2952e0ed992de132d1a4c
Sergey <sergey@mac.local>
2022-06-17 08:57:46 +0300

    first commit

---
+++ src/main/java/ru/smurov/cam_cam/CamCamApplication.java
@@ -0,0 +1,13 @@
+package ru.smurov.cam_cam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamCamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamCamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/smurov/cam_cam/CamCamApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.smurov.cam_cam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamCamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
